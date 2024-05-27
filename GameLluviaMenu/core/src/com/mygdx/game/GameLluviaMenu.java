c707a2334cdf3a342e567f5a73f3966136a8f3d6
maximilianoBustamante <142523209+maximilianoBustamante@users.noreply.github.com>
2024-05-27 19:09:30 -0400

    Modificar GameLluviaMenu para Usar las Nuevas Clases

--- GameLluviaMenu/core/src/com/mygdx/game/GameLluviaMenu.java
+++ GameLluviaMenu/core/src/com/mygdx/game/GameLluviaMenu.java
@@ -4,42 +4,39 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-	public class GameLluviaMenu extends Game {
+public class GameLluviaMenu extends Game {
+    private SpriteBatch batch;
+    private BitmapFont font;
+    private int higherScore;
 
-		private SpriteBatch batch;
-		private BitmapFont font;
-		private int higherScore;
+    public void create() {
+        batch = new SpriteBatch();
+        font = new BitmapFont(); // use libGDX's default Arial font
+        this.setScreen(new MainMenuScreen(this));
+    }
 
-		public void create() {
-			batch = new SpriteBatch();
-			font = new BitmapFont(); // use libGDX's default Arial font
-			this.setScreen(new MainMenuScreen(this));
-		}
+    public void render() {
+        super.render(); // important!
+    }
 
-		public void render() {
-			super.render(); // important!
-		}
+    public void dispose() {
+        batch.dispose();
+        font.dispose();
+    }
 
-		public void dispose() {
-			batch.dispose();
-			font.dispose();
-		}
+    public SpriteBatch getBatch() {
+        return batch;
+    }
 
-		public SpriteBatch getBatch() {
-			return batch;
-		}
+    public BitmapFont getFont() {
+        return font;
+    }
 
-		public BitmapFont getFont() {
-			return font;
-		}
+    public int getHigherScore() {
+        return higherScore;
+    }
 
-		public int getHigherScore() {
-			return higherScore;
-		}
-
-		public void setHigherScore(int higherScore) {
-			this.higherScore = higherScore;
-		}
-		
-
-	}
+    public void setHigherScore(int higherScore) {
+        this.higherScore = higherScore;
+    }
+}
